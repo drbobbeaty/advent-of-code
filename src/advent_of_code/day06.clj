4f2d5369bb7d8b3cb9c13a299dd9f962ffffc182
Bob Beaty <drbob@themanfromspud.com>
2015-12-06 18:22:13 -0600

    Refactored the code to use rules
    With a simple rule function, we can collapse the code into one function that
    runs a pight through the complete plan, and another that aggregates the
    lights into a board. This is a lot nicer as it's clear what's different
    about the two approaches.

--- src/advent_of_code/day06.clj
+++ src/advent_of_code/day06.clj
@@ -14,55 +14,43 @@
           :when (and x1 y1 x2 y2)]
       [chg [(pint x1) (pint y1)] [(pint x2) (pint y2)]])))
 
+(defn elves
+  "These are the rules the elves started with."
+  [sc lit]
+  (case sc
+    :on  1
+    :off 0
+    :toggle (mod (inc lit) 2)))
+
+(defn santa
+  "These are the rules that Santa corrected."
+  [sc lit]
+  (case sc
+    :on  (inc lit)
+    :off (max 0 (dec lit))
+    :toggle (+ 2 lit)))
+
 (defn light
-  "Function to find out the final state of any one light in the light board
-  based on it starting off, and applying Santa's plan for the lights. This
-  idea is the basic ray-tracing idea - follow one light through the process
-  and see what it's eventual value is."
-  [[x y] plan]
-  (loop [lit false
-         acts plan]
-    (if-let [sa (first acts)]
-      (let [[sc [x1 y1] [x2 y2]] sa
-            lit' (if (and (<= x1 x x2) (<= y1 y y2))
-                   (case sc
-                     :on  true
-                     :off false
-                     :toggle (not lit))
-                   lit)]
-        (recur lit' (rest acts)))
-      lit)))
-
-(defn count-lights
-  "Function to run through all the lights on the board, and count up the ones
-  that are on after all the operations are done."
-  [plan]
-  (count (filter #(light % plan) (for [row (range 1000)
-                                       col (range 1000)]
-                                   [row col]))))
-
-(defn dimmer
   "Function to follow the Ancient Elvish plans and assume the lights aren't
   just on or off, but they have a variable value that can be no less than 0,
-  but can go very high."
-  [[x y] plan]
+  but can go very high. The `rule` tells us what the next state of the light
+  is given the current state, and the transition from the directions."
+  [[x y] plan rule]
   (loop [lit 0
          acts plan]
     (if-let [sa (first acts)]
       (let [[sc [x1 y1] [x2 y2]] sa
             lit' (if (and (<= x1 x x2) (<= y1 y y2))
-                   (case sc
-                     :on  (inc lit)
-                     :off (max 0 (dec lit))
-                     :toggle (+ 2 lit))
+                   (rule sc lit)
                    lit)]
         (recur lit' (rest acts)))
       lit)))
 
 (defn count-lumens
   "Function to run through all the lights on the board, and this time, count
-  up the lumens of the board - one light at a time."
-  [plan]
-  (reduce + (map #(dimmer % plan) (for [row (range 1000)
-                                       col (range 1000)]
-                                   [row col]))))
+  up the lumens of the board - one light at a time. The `rule` is either the
+  `elves` or `santa` for part 1 or 2 of the advent challenge."
+  [plan rule]
+  (reduce + (map #(light % plan rule) (for [row (range 1000)
+                                             col (range 1000)]
+                                         [row col]))))
