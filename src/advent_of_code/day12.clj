21b4bca88dbc8b39de32d35a2d25fd183ffebbdf
Bob Beaty <drbob@themanfromspud.com>
2015-12-13 17:09:30 -0600

    Now I like this solution
    This is what I was looking for - something that used just the one `sum`
    function, and an override for the killing of the summing based on a value.
    This is just what I was hoping to come up with. I'm glad I kept with it.

--- src/advent_of_code/day12.clj
+++ src/advent_of_code/day12.clj
@@ -6,30 +6,32 @@
   "This is the input from the site for Santa's books."
   (json/parse-string (slurp "resources/input/day12.json") true))
 
+(def ^:dynamic *kill* nil)
+
 (defn sum
   "Function to sum all the values in the provided input - accoring to Santa's
   rules."
   [x]
-  (cond
-    (nil? x) 0
-    (string? x) 0
-    (number? x) x
-    (keyword? x) 0
-    (map? x) (if (empty? x) 0 (apply + (for [[k v] x] (+ (sum k) (sum v)))))
-    (coll? x) (if (empty? x) 0 (apply + (map sum x)))
-    :else 0))
+  (let [done? (fn [x] (or (empty? x)
+                          (and *kill* (<= 0 (.indexOf (vals x) *kill*)))))]
+    (cond
+      (nil? x) 0
+      (string? x) 0
+      (number? x) x
+      (keyword? x) 0
+      (map? x) (if (done? x) 0 (apply + (for [[k v] x] (+ (sum k) (sum v)))))
+      (coll? x) (if (empty? x) 0 (apply + (map sum x)))
+      :else 0)))
+
+(defmacro with-kill
+  "Macro to set up the dynamic kill value variable for the processing."
+  [kk & body]
+  `(binding [*kill* ~kk]
+     ~@body))
 
 (defn sum-red
   "Function to sum all the values in the provided input - accoring to Santa's
   rules. But this time, we exclude the maps with 'red' in the values."
   [x]
-  (cond
-    (nil? x) 0
-    (string? x) 0
-    (number? x) x
-    (keyword? x) 0
-    (map? x) (if (or (empty? x)
-                     (<= 0 (.indexOf (vals x) "red"))) 0 (apply + (for [[k v] x] (+ (sum k) (sum v)))))
-    (coll? x) (if (empty? x) 0 (apply + (map sum x)))
-    :else 0))
-
+  (with-kill "red"
+    (sum x)))
