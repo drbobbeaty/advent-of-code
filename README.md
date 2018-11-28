8c3d7af3395f752621cca6b05d3469b21d5ee39e
Bob Beaty <drbob@themanfromspud.com>
2018-11-28 14:48:49 -0600

    Finished Day 08
    This was interesting, as I wanted to try and make it more clever by
    working with the internal storage, but that wasn't the point - make it
    simple and easy and work. Much better.

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
-# advent-of-code
+# Advent of Code
 
 Just my solutions to the problems at http://adventofcode.com
 
+## 2015
+
+This was my first year and I made it all the way to Day 21 - it was a lot of
+fun to do some of these days. And I have to admit that Clojure was just a great
+tool to get a lot of these puzzles done.
+
+## 2016
+
+This year I was a little _busier_ and I only got to Day 8 - and I know it was
+just that I _wanted_ to do it, but a lot of the puzzles had state in them, and
+that wasn't a great fit for Clojure. But it was still fun to work these with
+a friend.
+
+## 2017
+
+This I didn't really get started on, and I feel kinda bad about that. But I'm
+hoping to take some time and really see if I can do a few of these while I'm
+also working on this year's puzzles.
+
+## 2018
+
+Starting soon...
+
 ## License
 
-Copyright © 2015 Bob Beaty
+Copyright © 2015-2018 Bob Beaty
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- src/advent_of_code/2016/day08.clj
+++ src/advent_of_code/2016/day08.clj
@@ -38,7 +38,7 @@
 
 (defn build
   "Function to create the 'display' with the provided width and height. This
-  willl destroy the display that is already there, and the new display will be
+  will destroy the display that is already there, and the new display will be
   reset to all 'off'."
   [w h]
   (let [row (vec (repeat w 0))]
@@ -66,13 +66,34 @@
           y (range h)]
     (dset y x 1)))
 
+(defn rotate-col
+  "Function to rotate the specified column index one display bit down. This
+  will then be used to build up the more general `rotate` function, below."
+  [idx]
+  (let [rows (count @display)
+        lg (dget (dec rows) idx)]
+    (doseq [r (range (dec rows) 0 -1)]
+      (dset r idx (dget (dec r) idx)))
+    (dset 0 idx lg)))
+
+(defn rotate-row
+  "Function to rotate the specified row index one display bit right. This
+  will then be used to build up the more general `rotate` function, below."
+  [idx]
+  (let [cols (count (first @display))
+        lg (dget idx (dec cols))]
+    (doseq [c (range (dec cols) 0 -1)]
+      (dset idx c (dget idx (dec c))))
+    (dset idx 0 lg)))
+
 (defn rotate
-  ""
+  "Function to execute the 'rotate' command on the 'display' with the parameters
+  for the call in the provided map."
   [{ax :axis idx :index amt :amount}]
   (cond
-    (= "column" ax)
-    (= "row" ax)
-    ))
+    (= "column" ax) (doseq [s (range amt)] (rotate-col idx))
+    (= "row" ax)    (doseq [s (range amt)] (rotate-row idx))
+    :else           nil))
 
 (defn cnt-pixels
   "Function to count the pixels in the 'display' that have the state provided -
@@ -85,29 +106,39 @@
          (apply +))))
 
 (defn show
-  ""
+  "Function to simply make a decent looking output for the 'display' based on
+  the state of that display in the internal logic of the code."
   []
   (doseq [r @display]
-    (println (apply str (map #(if (pos? %) "#" ".") r)))
-    ))
+    (println (apply str (map #(if (pos? %) "#" ".") r)))))
 
 (defn one
   "Function to take all the commands from the `puzzle` and run them over a 50x6
   display and then count up the number of pixels that are 'lit'."
-  [cmds]
+  [& [cmds]]
   (build 50 6)
-  (doseq [c cmds
+  (doseq [c (or cmds puzzle)
           :let [dc (:action c)]]
     (dc c))
   (cnt-pixels true))
 
+(defn two
+  "Function to take all the commands from the `puzzle` and run them over a 50x6
+  display and then display it to see what it's trying to say to us."
+  [& [cmds]]
+  (build 50 6)
+  (doseq [c (or cmds puzzle)
+          :let [dc (:action c)]]
+    (dc c))
+  (show))
+
 (defn yoyo
-  ""
+  "Function to run the simple example in the puzzle to see that we have all the
+  parts working properly."
   []
   (build 7 3)
   (let [src ["rect 3x2" "rotate column x=1 by 1" "rotate row y=0 by 4" "rotate column x=1 by 1"]]
     (doseq [c (code-it src)
             :let [dc (:action c)]]
       (dc c))
-    (show)
-    ))
+    (show)))
