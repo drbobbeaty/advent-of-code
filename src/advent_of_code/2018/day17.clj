9f8a9797d7b9fedf4bd0e4d3a952a4346bc910ff
Bob Beaty <drbob@themanfromspud.com>
2018-12-18 17:59:03 -0600

    Have a workable solution for Day 17
    This is now a workable solution for Day 17 - there are some efficiency
    issues that could be addressed, and the real problem is that while the
    solution appears to be right on the money, they AoC site says it's too
    high. This is because there is some rule I'm not correctly implementing,
    but this solution is so much simpler than the previous one, it's hard to
    imagine what it is. Again... the solution looks correct, and stable. So
    this is likely to never be really solved.

--- src/advent_of_code/2018/day17.clj
+++ src/advent_of_code/2018/day17.clj
@@ -1,5 +1,5 @@
 (ns advent-of-code.2018.day17
-  "Sixteenth day's solutions for the Advent of Code 2018"
+  "Seventeenth day's solutions for the Advent of Code 2018"
   (require [clojure.java.io :as io]
            [clojure.string :as cs]
            [clojure.tools.logging :refer [error errorf info infof warnf debugf]]))
@@ -43,7 +43,10 @@
 
 (def scan (atom []))
 (def xoffset (atom 0))
+(def xlimit (atom 0))
 (def ylimit (atom 0))
+
+(def stopper (atom 0))
 
 (defn sget
   "Function to get the value of the scan at `y` (row), and `x` (column). If
@@ -68,9 +71,10 @@
   (let [clay (code-it s)
         [xl xh] (apply (juxt min max) (map first clay))
         [yl yh] (apply (juxt min max) (map second clay))
-        xos (- xl 10)
-        row (vec (repeat (+ 20 (- xh xl)) \.))]
+        xos (- xl 20)
+        row (vec (repeat (+ 40 (- xh xl)) \.))]
     (reset! xoffset xos)
+    (reset! xlimit (+ 20 xh))
     (reset! ylimit yh)
     (reset! scan (vec (repeat (inc yh) row)))
     (sset 500 0 \+)
@@ -88,18 +92,14 @@
   "Function to make a nice log message for the provided scan. This
   will make it easy to see what's happening."
   []
-  (let [fmt (fn [s] (format "%s" (apply str s)))]
-    (info (str "scan:\n" (cs/join "\n" (map fmt @scan))))))
+  (let [fmt (fn [[i s]] (format "[%4d] %s" i (apply str s)))]
+    (info (str "scan:\n" (cs/join "\n" (map fmt (map vector (range) @scan)))))))
 
 (defn wet
-  ""
+  "Function to count the number of wet cells in the scan - they could be
+  water (~) or they counld have once had water (|)."
   []
-  (let [cnt (atom 0)]
-    (doseq [row @scan
-            c row
-            :when (#{\~ \|} c)]
-      (swap! cnt inc))
-    @cnt))
+  (apply + (map #(count (filter #{\~ \|} %)) @scan)))
 
 (defn step
   ""
@@ -111,63 +111,47 @@
 
 (declare run drip)
 
+(defn edge
+  "Function to find the edge, starting from the point, and moving in the
+  incremental direction provided, we look for a wall, or a cliff, and return
+  the x value and condition of that junction."
+  [sx sy dx]
+  (loop [x sx]
+    (if (<= @xoffset x @xlimit)
+      (let [[nx ny] [(+ dx x) sy]
+            nv (sget nx ny)
+            nvd (sget nx (inc ny))
+            ans (cond
+                  (= \# nv) [x :wall]
+                  (and (#{\. \|} nv) (#{\~ \#} nvd)) nil
+                  (and (#{\. \|} nv) (#{\. \|} nvd)) [nx :spill])]
+        (if (nil? ans)
+          (recur nx)
+          ans))
+      [x :edge])))
+
 (defn flow
   ""
-  ([sx sy]
-    (infof "flow: x,y = %s, %s" sx sy)
-    (let [spc (sget sx sy)
-          cr (sget (inc sx) sy)
-          cl (sget (dec sx) sy)
-          slide (fn [mfn]
-                  (loop [x' sx]
-                    (let [cr (sget (mfn x') sy)]
-                      (cond
-                        (= \. cr) (drip x' sy)
-                        (= \| cr) (recur (mfn x'))))))
-         ]
-      ; (if (and (= \| spc) (#{\| \#} cr) (#{\| \#} cl))
-      (if (= \| spc cr)
-        (loop [x' sx]
-          (let [cr (sget (inc x') sy)]
-            (cond
-              (= \. cr) (drip x' sy)
-              (= \| cr) (recur (inc x'))))))
-      (if (= \| spc cl)
-        (loop [x' sx]
-          (let [cl (sget (dec x') sy)]
-            (cond
-              (= \. cl) (drip x' sy)
-              (= \| cl) (recur (dec x'))))))
-      (when (and (not= \| cr) (not= \| cl))
-        (sset sx sy \.)
-        (flow sx sy :right)
-        (flow sx sy :left))))
-  ([sx sy dir]
-    (let [spc (sget sx sy)
-          ofc (if (= spc \|) \| \~)]
-      (loop [x sx
-             y sy]
-        (let [v (sget x y)
-              dn (sget x (inc y))
-              [nx ny] (step [x y] dir)
-              go? (cond
-                    (= \# v) false
-                    (= \~ v) true
-                    (#{\. \|} v)
-                      (cond
-                        (= \. dn) (let [dx (if (= :left dir) 1 -1)]
-                                    (loop [x' sx]
-                                      (when (= \~ (sget x' y))
-                                        (sset x' y \|)
-                                        (recur (+ x' dx))))
-                                    (doseq [x' (apply range (sort [sx x]))]
-                                      (sset x' y \|))
-                                    (drip x y))
-                        (#{\# \~} dn) (do (sset x y ofc) true)
-                      )
-                    )]
-          (if go? (recur nx ny))
-        ))
+  [sx sy]
+  ; (infof "flow: x,y = %s, %s" sx sy)
+  (let [[rx rb] (edge sx sy 1)
+        [lx lb] (edge sx sy -1)
+       ]
+    ; (infof "[flow] y=%s L: %s ... R: %s" sy [lx lb] [rx rb])
+    ; (if (= 390 sx) (infof "[flow(x)] y=%s L: %s ... R: %s ... '%s'" sy [lx lb] [rx rb] (sget sx sy)))
+    (cond
+      (= :wall rb lb)
+        (doseq [x' (range lx (inc rx))] (sset x' sy \~))
+      (or (= :spill rb) (= :spill lb))
+        (do
+          (doseq [x' (range lx (inc rx))] (sset x' sy \|))
+          ; (when (= :spill rb) (infof "[flow] dripping (R) to %s, %s" rx sy) (drip rx sy))
+          ; (when (= :spill lb) (infof "[flow] dripping (L) to %s, %s" lx sy) (drip lx sy))
+          (if (= :spill rb) (drip rx sy))
+          (if (= :spill lb) (drip lx sy))
+        )
+      (or (= :edge rb) (= :edge lb))
+        false
       )
     false
     ))
@@ -175,53 +159,66 @@
 (defn drip
   ""
   [& [sx sy]]
-  (infof "drip: x,y = %s, %s" sx sy)
+  ; (infof "[%s] drip: x,y = %s, %s = '%s' & '%s'" @stopper sx sy (if (and sx sy) (str (sget sx sy))) (if (and sx sy) (str (sget sx (inc sy)))))
   (loop [x (or sx 500)
-         y (or sy 0)
-         dir :down]
+         y (or sy 0)]
     (if (<= y @ylimit)
+    ; (if (and (< @stopper 1000) (<= y 1644)) ;;(<= y @ylimit)
       (let [v (sget x y)
-            [nx ny] (step [x y] dir)
-            nv (sget nx ny)
-            go? (cond
-                  (= \+ v) true
-                  (= \. v) (case nv
-                             (\. \|) (do (sset x y \|) true)
-                             (\# \~) (flow x y)
-                           )
-                  (= \| v) (case nv
-                             \.      (do (sset x y \|) true)
-                             \|      true
-                             (\# \~) (flow x y)
-                           )
-                  )
-           ]
+            ny (inc y)
+            nv (sget x ny)
+            go? (case v
+                  \+ true
+                  \. (case nv
+                       (\. \|) (do (sset x y \|) true)
+                       (\# \~) (do
+                                 ; (infof "[drip(.)] hit bottom: %s, %s w/ '%s' on '%s'" x y v nv)
+                                 (flow x y)
+                                )
+                     )
+                  \| (case nv
+                       \.      (do (sset x y \|) true)
+                       \|      true
+                       (\# \~) (do
+                                 ; (infof "[drip(|)] hit bottom: %s, %s w/ '%s' on '%s'" x y v nv)
+                                 (flow x y)
+                                )
+                     )
+                  )]
         (if go?
-          (recur nx ny dir))
+          (recur x ny)))
+      (do
+        (when (zero? (mod @stopper 5000))
+          (infof "stopper=%s ... wet=%s" @stopper (wet))
+          (slog)
+        )
+        ; (infof "[drip(%s)] hit limit: %s, %s w/ '%s' on '%s'" @stopper x y (if (and sx sy) (str (sget sx sy))) (if (and sx sy) (str (sget sx (inc sy)))))
+        (swap! stopper inc)
       )
-    )
-  )
-  )
+      )))
 
 (defn run
   ""
   [& [sx sy]]
   (loop []
-    (let [curr (wet)]
+    (let [curr @scan]
       (drip sx sy)
-      (if-not (= curr (wet))
-        (recur))
-    )
-  ))
+      (if-not (= curr @scan)
+        (recur)))))
 
 (defn one
-  ""
+  "Got to 37077 and everything looks right, but AoC says that is too high.
+  They have not completely specified the rules, and that's what the issue
+  really is."
   []
   (build puzzle)
-  (doseq [i (range 500)]
+  (doseq [i (range 200)]
+    (reset! stopper 0)
     (drip))
   ; (run)
   (slog)
+  ; (drip)
+  ; (slog)
   (wet))
 
 (defn yoyo
@@ -230,15 +227,7 @@
   []
   (let [x 5
         ]
-    (build sample)
+    (build puzzle)
     (run)
-    ; (drip)
-    ; (drip)
-    ; (drip)
-    ; (drip)
-    ; (drip)
-    ; (drip)
-    ; (drip)
-    ; (drip)
     (slog)
     (wet)))
