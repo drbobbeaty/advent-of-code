630b62e341d1de8dc1bf8d4ae60af39433a3af50
Bob Beaty <drbob@themanfromspud.com>
2018-12-20 11:45:12 -0600

    Parsing of Day 20 path
    This is a first stab at part 1 - where I had to really make a regex
    engine for the pathing data, and I was hoping to get a reasonable
    solution to the problem by parsing the path, but it really looks like
    you have to complete the layout, and then find the room, and then a
    path, and that's more than I can do today.

---
+++ src/advent_of_code/2018/day20.clj
@@ -0,0 +1,84 @@
+(ns advent-of-code.2018.day20
+  "Twentieth day's solutions for the Advent of Code 2018"
+  (require [advent-of-code.2016.day25 :refer [->int]]
+           [clojure.java.io :as io]
+           [clojure.string :as cs]
+           [clojure.tools.logging :refer [error errorf info infof warnf debugf]]))
+
+(def puzzle
+  "This is the input of the forest."
+  (-> (slurp "resources/2018/input/day19.txt")
+      (cs/split #"\n")))
+
+(defn chop
+  "Function to chop up the string into parts that are direct, and those that
+  are optional. This optionality could mean that there are multiple levels of
+  optionality, and those have to be delt with, but this is chopping it up at
+  a high level."
+  [s]
+  (let [ts (if (and (.startsWith s "^") (.endsWith s "$")) (.subSequence s 1 (dec (count s))) s)]
+    (loop [src ts
+           prts []]
+      (let [idx (.indexOf src "(")]
+        (if-not (neg? idx)
+          (let [hdr (if (pos? idx) (subs src 0 idx))
+                edx (loop [i (inc idx)
+                           lvl 0]
+                      (case (nth src i nil)
+                        (\N \E \S \W \|) (recur (inc i) lvl)
+                        \( (recur (inc i) (inc lvl))
+                        \) (if (zero? lvl) i (recur (inc i) (dec lvl)))
+                        nil i))
+                ftr (not-empty (subs src idx (inc edx)))]
+            (recur (subs src (inc edx)) (conj prts hdr ftr)))
+          (if (not-empty src) (conj prts src) prts))))))
+
+(defn fork
+  "Function to fork apart the optional parts of a regex, and return them as
+  a sequence of parts that can, in turn be explicit parts and optional parts."
+  [s]
+  (let [ts (if (and (.startsWith s "(") (.endsWith s ")")) (.subSequence s 1 (dec (count s))) s)]
+    (loop [src ts
+           prts []]
+      (let [idx (loop [i 0
+                       lvl 0]
+                  (case (nth src i nil)
+                    (\N \E \S \W) (recur (inc i) lvl)
+                    \( (recur (inc i) (inc lvl))
+                    \) (recur (inc i) (dec lvl))
+                    \| (if (zero? lvl) i (recur (inc i) lvl))
+                    nil -1))]
+        (if-not (neg? idx)
+          (let [hdr (if (pos? idx) (subs src 0 idx))]
+            (recur (subs src (inc idx)) (conj prts hdr)))
+          (conj prts src))))))
+
+(defn shortest
+  ""
+  [s]
+  (let [opt? (fn [s] (and (.startsWith s "(") (.endsWith s ")")))
+        dir? (fn [s] (neg? (.indexOf s "|")))]
+    (cond
+      (dir? s) (count s)
+      (opt? s) (vec (map shortest (fork s)))
+      :else (map shortest (chop s))
+      )
+  ))
+
+(defn one
+  ""
+  []
+  nil)
+
+(defn yoyo
+  "Function just to test out the example and make sure we have the tools
+  working right."
+  []
+  (let [src ["^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+             "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
+             "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"
+            ]
+      ]
+    {:src (second src)
+     :path (shortest (second src))}
+  ))
