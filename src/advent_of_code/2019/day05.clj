08876e13f60852de1cdd76218012f2bdb35f3d92
Bob Beaty <drbob@themanfromspud.com>
2019-12-07 07:48:09 -0600

    Added I/O pausing to run function
    In order to handle Day 7, we needed to handle I/O pausing of the
    execution. The easiest way to do that was to make the complete machine
    state a map, and then allow for either the old-style args, or the new
    state map, and then update the state on output - consuming inputs, and
    appending outputs. The pausing for I/O was needed due to Day 7, part 2,
    where the output of one program had to be fed into the next amplifier as
    input - but without losing where each program was. Not bad... :)

--- src/advent_of_code/2019/day05.clj
+++ src/advent_of_code/2019/day05.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code.2019.day05
   "Fifth day's solutions for the Advent of Code 2019"
-  (:require [advent-of-code.util :refer [parse-int not-zero?]]
+  (:require [advent-of-code.util :refer [parse-int not-zero? mk-seq un-seq
+                                         parse-bool]]
             [advent-of-code.2019.day02 :refer [vat]]
             [clojure.string :as cs]))
 
@@ -24,35 +25,41 @@
   Intcode program starting at that address and ending when the program
   successfully exits."
   [mems & [arg pc]]
-  (loop [mem mems
-         ip (or pc 0)]
-    (let [inst (nth mem ip nil)
-          op (mod inst 100)
-          mde (reverse (drop-last 2 (str inst)))
-          ld (fn [ipos]
-               (if (= \1 (nth mde (dec ipos) \0))
-                 (nth mem (+ ipos ip) nil)
-                 (vat mem (+ ipos ip))))]
-      (case op
-        (1 2) (let [a (ld 1)
-                    b (ld 2)
-                    tgt (nth mem (+ 3 ip) nil)]
-                (recur (assoc mem tgt ((if (= op 1) + *) a b)) (+ 4 ip)))
-        3     (let [inp (or arg 0)
-                    tgt (nth mem (+ 1 ip) nil)]
-                (recur (assoc mem tgt inp) (+ 2 ip)))
-        4     (let [out (ld 1)]
-                (prn out)
-                (recur mem (+ 2 ip)))
-        (5 6) (let [tst (ld 1)
-                    nip (ld 2)]
-                (recur mem (if ((if (= op 5) not-zero? zero?) tst) nip (+ 3 ip))))
-        (7 8) (let [a (ld 1)
-                    b (ld 2)
-                    tgt (nth mem (+ 3 ip) nil)]
-                (recur (assoc mem tgt (if ((if (= op 7) < =) a b) 1 0)) (+ 4 ip)))
-        99    mem
-        mem))))
+  (let [no-io? (not (parse-bool (:io-wait mems)))]
+    (loop [mem (vec (if (map? mems) (:memory mems) mems))
+           ip (or (if (map? mems) (:pc mems) pc) 0)
+           ins (if-let [v (if (map? mems) (:input mems) arg)] (mk-seq v))
+           outs (transient (if (map? mems) (vec (:output mems)) []))]
+      (let [inst (nth mem ip nil)
+            op (mod inst 100)
+            mde (reverse (drop-last 2 (str inst)))
+            ld (fn [ipos]
+                 (if (= \1 (nth mde (dec ipos) \0))
+                   (nth mem (+ ipos ip) nil)
+                   (vat mem (+ ipos ip))))]
+        (case op
+          (1 2) (let [a (ld 1)
+                      b (ld 2)
+                      tgt (nth mem (+ 3 ip) nil)]
+                  (recur (assoc mem tgt ((if (= op 1) + *) a b)) (+ 4 ip) ins outs))
+          3     (let [inp (first ins)
+                      tgt (nth mem (+ 1 ip) nil)]
+                  (if (or inp no-io?)
+                    (recur (assoc mem tgt (or inp 0)) (+ 2 ip) (rest ins) outs)
+                    {:output (persistent! outs) :memory mem :input nil :pc ip :state :io-in}))
+          4     (let [ov (ld 1)]
+                  (if no-io?
+                    (recur mem (+ 2 ip) ins (conj! outs ov))
+                    {:output (persistent! (conj! outs ov)) :memory mem :input ins :pc (+ 2 ip) :state :io-out}))
+          (5 6) (let [tst (ld 1)
+                      nip (ld 2)]
+                  (recur mem (if ((if (= op 5) not-zero? zero?) tst) nip (+ 3 ip)) ins outs))
+          (7 8) (let [a (ld 1)
+                      b (ld 2)
+                      tgt (nth mem (+ 3 ip) nil)]
+                  (recur (assoc mem tgt (if ((if (= op 7) < =) a b) 1 0)) (+ 4 ip) ins outs))
+          99    {:output (persistent! outs) :memory mem :input ins :pc ip :state :halt}
+          {:output (persistent! outs) :memory mem :input ins :pc ip :state :exception})))))
 
 (defn yoyo
   "Run a bunch of tests on the trial input. The output should be a series of
@@ -77,7 +84,8 @@
                        1106 0 36 98 0 0 1002 21 125 20 4 20 1105 1 46 104
                        999 1105 1 46 1101 1000 1 20 4 20 1105 1 46 98 99] 9]
                      ]]
-    (run mem inp)))
+    (prn (un-seq (:output (run mem inp))))
+    ))
 
 (defn bobo
   "Testing out the one simluation looking for an issue. Got it!"
@@ -89,13 +97,11 @@
   the end because the printing output will be the log of all we need."
   [& coll]
   (let [inp (vec (or coll puzzle))]
-    (run inp 1)
-    :done))
+    (:output (run inp 1))))
 
 (defn two
   "Function to run the program with an input of 5, and toss the memory at
   the end because the printing output will be the log of all we need."
   [& coll]
   (let [inp (vec (or coll puzzle))]
-    (run inp 5)
-    :done))
+    (:output (run inp 5))))
