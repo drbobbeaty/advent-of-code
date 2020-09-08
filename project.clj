b6687a4a4d3958d3f23b9c224a48d6fd5915bb22
Bob Beaty <bob@bobbeaty.com>
2020-09-08 13:02:02 +0000

    Added whiskey to the project.clj for GitPod
    Given that GitHub is piloting a GitPod alternative, I wanted to get this
    into the project, as I really like this for working on the Advent of
    Code problems. It'll be interesting to see how the two compare. And this
    Will be a great test case for this work.

--- project.clj
+++ project.clj
@@ -18,4 +18,20 @@
                  [cheshire "5.6.3"]
                  [clojure.java-time "0.3.2"]
                  [pandect "0.5.4"]]
-  :jvm-opts ["-Xmx1g" "-Xss500m"])
+  :jvm-opts ["-Xmx1g" "-Xss500m"]
+  :profiles {:dev {;; whidbey is for nice printing and coloring of the REPL output
+                   :plugins [[mvxcvi/whidbey "2.2.1"]]
+                   :middleware [whidbey.plugin/repl-pprint]
+                   :whidbey {:color-scheme {:delimiter [:bold :yellow]
+                                            :tag [:bold :yellow]
+                                            :nil [:cyan]
+                                            :boolean [:bold :cyan]
+                                            :number [:bold :green]
+                                            :string [:bold :red]
+                                            :character [:cyan]
+                                            :keyword [:yellow]
+                                            :symbol [:bold :magenta]
+                                            :function-symbol [:bold :blue]
+                                            :class-delimiter [:blue]
+                                            :class-name [:green]
+                                            :exception [:bold :red]}}}})
