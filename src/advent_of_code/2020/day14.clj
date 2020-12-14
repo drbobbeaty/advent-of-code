850ad5a8ab9ff53ff15df8626893fd497f3aaabc
Bob Beaty <drbob@themanfromspud.com>
2020-12-14 09:11:37 -0600

    Finished Day 14
    Part 2 was just a lot of little housekeeping on the matching of the Xs
    in the pask to increasing binary numbers. But once we got it broken
    down, it was not too bad. But a lot of code - I'm sure there have to be
    simpler solutions.

--- src/advent_of_code/2020/day14.clj
+++ src/advent_of_code/2020/day14.clj
@@ -37,41 +37,85 @@
        "mem[26] = 1"]
       (->> (map parse))))
 
-(defn band
-  "Function to perform a bit-wise forcing of the value and the bitmask provided.
-  In the mask, an 'X' is nothing, a '1' and '0' are set on the value's bits, and
-  the decimal value is returned. It's a bit like AND/OR, but just forcing the
-  values, as they appear in the mask."
-  [v bm]
-  (let [bv (str "000000000000000000000000000000000000" (Long/toString v 2))]
-    (-> (for [[i t] (map vector (reverse bv) (reverse bm))]
-          (if (= t \X) i t))
-        (reverse)
-        (->> (apply str))
-        (Long/parseLong 2))))
+(defn mask-val
+  "Function to update the memory with the provided command and mask. This will
+  perform a bit-wise forcing of the value and the bitmask provided - as per the
+  instructions for part 1. In the mask, an 'X' is nothing, a '1' and '0' are
+  set on the value's bits, and the decimal value is returned. It's a bit like
+  AND/OR, but just forcing the values, as they appear in the mask. The return
+  value is the updated memory map."
+  [mem {adr :addr val :value :as cmd} mask]
+  (let [bv (str "000000000000000000000000000000000000" (Long/toString val 2))
+        nv (-> (for [[i t] (map vector (reverse bv) (reverse mask))]
+                 (if (= t \X) i t))
+               (reverse)
+               (->> (apply str))
+               (Long/parseLong 2))]
+    (assoc mem adr nv)))
 
 (defn exec
   "Function to take a memory layout - address and values as the key/value pairs,
   and then a sequence on mask/memory commands that need to be executed against
   that memory space, and the result returned to the caller."
-  [m pgm]
+  [m pgm updfn]
   (loop [cmds pgm
          mem m
          mask nil]
     (if-let [c (first cmds)]
       (if (contains? c :mask)
         (recur (rest cmds) mem (:mask c))
-        (recur (rest cmds) (assoc mem (:addr c) (band (:value c) mask)) mask))
+        (recur (rest cmds) (updfn mem c mask) mask))
       mem)))
 
 (defn one
   "Function to run through the code, masking and setting values, and then
   summing up all the memory values we have and returning that."
   [& [m]]
-  (sum (vals (exec {} puzzle))))
+  (sum (vals (exec {} puzzle mask-val))))
+
+(def rev-bits
+  "A sequence of numbers - but in a unique format. These will be the binary
+  representation of each number, in order, but with the bits reversed so that
+  they can more easily be put into the floating positions in the address space."
+  (for [i (range 1024)]
+    (-> (str "000000000000000000000000000000000000" (Long/toString i 2))
+        (reverse)
+        (->> (take 36)))))
+
+(defn mk-addr
+  "Function to take the source from part 2 - with 'X' and digits, in the
+  reverse order, and create a sequence of decimal numbers that are the address
+  values for the Xs being replaced by increasing binary numbers - regardless of
+  their location. This is done reversed, to make sure that we start with the
+  smallest inserted number, and move up."
+  [arg]
+  (let [fs (take (int (Math/pow 2 (count (filter #(= \X %) arg)))) rev-bits)]
+    (for [f fs]
+      (loop [src arg
+             fbs f
+             ans (transient [])]
+        (if-let [c (first src)]
+          (if (= \X c)
+            (recur (rest src) (rest fbs) (conj! ans (first fbs)))
+            (recur (rest src) fbs (conj! ans c)))
+          (Long/parseLong (apply str (reverse (persistent! ans))) 2))))))
+
+(defn float-val
+  "Function to update the memory with the provided command and mask. This will
+  perform a bit-wise forcing of the value and the bitmask provided - as per the
+  instructions for part 1. In the mask, an 'X' is nothing, a '1' and '0' are
+  set on the value's bits, and the decimal value is returned. It's a bit like
+  AND/OR, but just forcing the values, as they appear in the mask. The return
+  value is the updated memory map."
+  [mem {adr :addr val :value :as cmd} mask]
+  (let [ba (str "000000000000000000000000000000000000" (Long/toString adr 2))
+        src (for [[i t] (map vector (reverse ba) (reverse mask))]
+              (if (= t \0) i t))]
+    (apply assoc mem (interleave (mk-addr src) (repeat val)))))
 
 (defn two
-  "Function to "
+  "Function to complete part 2 where we look at the masks as different rules,
+  and the addresses are mapped out in the memory space. Just a lot of little
+  tests and housekeeping."
   [& [m]]
-
-  )
+  (sum (vals (exec {} puzzle float-val))))
